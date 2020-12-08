6a3b4d3c561c0f2d0d64c418e6d8d95e3d14e2d4
colbyfayock <fayock@gmail.com>
2020-12-08 15:00:24 -0500

    Step 4: Adding Applitools Eyes check to the app

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -8,4 +8,19 @@ context('Home', () => {
   it('should find the title of the homepage', () => {
     cy.get('h1').contains('Welcome');
   });
+
+  it('should verify the homepage looks as expected', () => {
+    cy.eyesOpen({
+      appName: 'My App',
+      testName: 'Homepage Check',
+    });
+
+    cy.eyesCheckWindow({
+      tag: 'App Window',
+      target: 'window',
+      fully: true
+    });
+
+    cy.eyesClose();
+  });
 });
